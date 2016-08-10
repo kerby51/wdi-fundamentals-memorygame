20f4978a4dbf81843292fb9fc564877f76754a01
Katharine Benefico <kbenefico@gmail.com>
2016-08-10 16:17:39 -0400

    created conditional statements for a match

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,13 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cardOne = "king";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "queen";
+if(cardOne === cardThree) {
+  alert("You've found a match!");
+}
+else if(cardTwo === cardFour) {
+  alert("You've found a match!")
+}
+else {
+  alert("Sorry, try again.")
+}
