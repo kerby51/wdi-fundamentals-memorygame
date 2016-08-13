7949d16d8137ffc8e33b9da3acdc47d2f9832c35
Katharine Benefico <kbenefico@gmail.com>
2016-08-13 16:16:51 -0400

    added more javascript, updated html/css

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,13 +1,38 @@
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "queen";
-if(cardOne === cardThree) {
-  alert("You've found a match!");
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [ ];
+var board = document.getElementById('game-board');
+
+function createBoard () {
+	for (var i=0; i<cards.length; i++) {
+	var cardDiv = document.createElement('div');
+	cardDiv.className = 'card';
+	cardDiv.setAttribute('data-card', cards[i]);
+	board.appendChild(cardDiv);
+	board.appendChild(cardDiv);
+    cardDiv.addEventListener('click', isTwoCards);
+	}
 }
-else if(cardTwo === cardFour) {
-  alert("You've found a match!")
+function isTwoCards() {
+  cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='https://a.fsdn.com/con/app/proj/vector-cards/screenshots/KH.png'>"; 
+	} else {
+		this.innerHTML = "<img src='http://www.madore.org/~david/images/cards/english/queen-hearts.png'>"; 
+	}
+    if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+  }
 }
-else {
-  alert("Sorry, try again.")
+function isMatch(cards) {
+    if (cards[0] === cards[1]) {
+    alert("You found a match! Woohoo");
+    } else {
+    alert("Sorry, try again. Whomp whomp");
+    }
 }
+function playAgain() {
+	location.reload();
+}
+createBoard();
